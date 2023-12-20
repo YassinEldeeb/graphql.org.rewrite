b7282f6a039729fd0cc4092581ba91bfbcf724d0
YassinEldeeb <yassineldeeb94@gmail.com>
2023-12-20 15:07:47 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -2,8 +2,14 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/((?!_next).*)', // Matches all paths except those starting with '_next'
-        destination: 'https://graphql-github-io-real.pages.dev/:path*', // Rewrite to Cloudflare Pages
+        source: '/:path*',
+        destination: 'https://graphql-github-io-real.pages.dev/:path*',
+        has: [
+          {
+            type: 'host',
+            value: '(www.)?graphql.org',
+          },
+        ],
       },
     ]
   },
